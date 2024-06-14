272854ff96082a34dbe53cdd907a720c55ee6151
studenthwangdongha <0623dh@naver.com>
2024-06-14 09:45:21 +0900

    처음

--- js/text.js
+++ js/text.js
@@ -2,7 +2,7 @@
 // span 요소 노드 가져오기
 const spanEl = document.querySelector("main h2 span");
 // 화면에 표시할 문장 배열
-const txtArr = ['Web Publisher', 'Front-End Developer', 'Web UI Designer', 'UX Designer', 'Back-End Developer'];
+const txtArr = ['웹 퍼블리셔ㄹ', 'Front-End Developer', 'Web UI Designer', 'UX Designer', 'Back-End Developer'];
 // 배열의 인덱스 초깃값
 let index = 0;
 // 화면에 표시할 문장 배열에서 텍스트를 하나 가져온 뒤, 배열로 만들기
